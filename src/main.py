5544ebe6c62c2b73bc64fd9307d6629b9b0abe73
Yoshida24 <trihead1.dev@gmail.com>
2024-07-14 11:08:51 +0900

    feat: Difyの評価機能をmake runのデフォルトコマンドに

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-import modules.ragas_util.hello.evaluate as evaluate
+# import modules.ragas_util.hello.evaluate as evaluate
 
-# import modules.ragas_util.dify.evaluate as evaluate
+import modules.ragas_util.dify.evaluate as evaluate
 
 
 def main():
-    evaluate.evaluate_const_param()
-    # evaluate.evaluate_dify()
+    # evaluate.evaluate_const_param()
+    evaluate.evaluate_dify()
 
 
 if __name__ == "__main__":
